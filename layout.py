c1db85ad4c3a38722985808a59d5c3c1964d7670
Taher Dohadwala <taher.dohadwala1@gmail.com>
2020-11-24 11:08:51 -0600

    functional layout.py (states is borked) and easier to experiment map.py

--- layout.py
+++ layout.py
@@ -2,8 +2,6 @@
 This script contains the page layout, UI, mapping, and callback code for the vis
 
 app.layout is the page layout with dcc components embedded inside
-
-
 """
 
 import pandas as pd
@@ -17,21 +15,29 @@ from datetime import date
 from urllib.request import urlopen
 import json
 
-
+# get geojson to plot US map with plotly
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    counties = json.load(response)
+    geojson = json.load(response)
 
-cases = pd.read_csv("covid_confirmed_usafacts.csv")
-deaths = pd.read_csv("covid_deaths_usafacts.csv")
-dates = list(cases.columns[4:])
+# Load data 
+counties = pd.read_pickle("final_county.gz",compression="gzip")
+states = pd.read_pickle("final_state.gz",compression="gzip")
+
+counties["countyFIPS"] = counties["countyFIPS"].astype(str)
+counties['countyFIPS'] = counties['countyFIPS'].apply(lambda x: ('0'+x if(len(x) == 4) else x))
+# Get date range and states list
+dates = counties["Date"].unique()
 drilldown_options = ["States","Counties"]
-states = list(pd.unique(cases["State"]))
+states = list(pd.unique(counties["State"]))
 [drilldown_options.append(x) for x in states]
 
+discrete_color = list(pd.unique(counties["Case Rate Color"]))
+discrete_color_map = {}
+for i in discrete_color:
+    discrete_color_map[i] = i
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 app.layout = html.Div([
     html.Div([
         html.Div([
@@ -57,6 +63,7 @@ app.layout = html.Div([
         html.Div([
         html.H4(children="Map Data:"),
         dcc.RadioItems(
+            id="map-data",
             options=[
             {'label': 'Covid-19 vs Mobility', 'value': 'covid_mobility'},
             {'label': 'Covid-19', 'value': 'covid'},
@@ -70,7 +77,7 @@ app.layout = html.Div([
             id="drilldown",
             options=[{"label":x,"value":x} for x in drilldown_options],
             multi=False,
-            value="States"
+            value="Counties"
         )
         ],className="three columns"),
     ],className="row",style={"padding":25}),
@@ -91,27 +98,55 @@ def update_display_date(date):
     Output('graph1', 'figure'),
     [Input('date-slider', 'value'),
      Input("drilldown","value"),
-     Input("cases-deaths","value")
+     Input("cases-deaths","value"),
+     Input("map-data","value")
      ])
-
-def update_figure(date_value,drilldown,cases_deaths):
-    current = dates[date_value]
-    if cases_deaths == "cases":
-        df = cases
-    else:
-        df = deaths
+def update_figure(date_value,drilldown,cases_deaths,map_data):
     print("Updating....")
+    current = dates[date_value]
+    data = ""
+    color_map = "continuous"
+    
+    if map_data == "covid":
+        if cases_deaths == "cases":
+            data = "Case growth rate"
+        else:
+            data = "Death growth rate"
+        
+    elif map_data == "mobility":
+        data = "Mobility"
+    else:
+        color_map = "discrete"
+        if cases_deaths == "cases":
+            data = "Case Rate Color"
+        else:
+            data = "Death Rate Color"
+        
+        
     if drilldown == "States":
-        sample = pd.DataFrame()
-        sample["stateFIPS"] = df["stateFIPS"].unique()
-        sample[current] =df[["stateFIPS",current]].groupby("stateFIPS").sum()[current]
-        sample.fillna(0)
-        loc = "stateFIPS"
-    else:        
-        sample = df[["countyFIPS", current]]
-        sample[current] = np.log10(sample[current])
-        loc = "countyFIPS"
-    fig = px.choropleth(sample, geojson=counties, locations=loc, color=current, scope="usa")
+        df = states[states["Date"] == current]
+        print(df.head())
+        if color_map == "discrete":
+            fig = px.choropleth(df,locationmode="USA-states",locations='State', color=data,color_discrete_map=discrete_color_map,scope="usa")
+        else:
+            fig = px.choropleth(df,locationmode="USA-states",locations='State', color=data,scope="usa")
+    elif drilldown == "Counties":
+        df = counties[counties["Date"] == current]
+        if color_map == "discrete":
+            fig = px.choropleth(df, geojson=geojson, locations="countyFIPS", color=data,color_discrete_map=discrete_color_map, scope="usa")
+        else:
+            fig = px.choropleth(df, geojson=geojson, locations="countyFIPS", color=data, scope="usa")
+
+    else:
+        df = counties[counties["Date"] == current]
+        df = df[df["State"] == drilldown]
+        if color_map == "discrete":
+            fig = px.choropleth(df, geojson=geojson, locations="countyFIPS", color=data,color_discrete_map=discrete_color_map)
+        else:
+            fig = px.choropleth(df, geojson=geojson, locations="countyFIPS", color=data)
+        fig.update_geos(fitbounds="locations", visible=False)
+        
+
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     print("Finished updating")
     return fig
--- map.py
+++ map.py
@@ -15,18 +15,52 @@ from urllib.request import urlopen
 import json
 
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    counties = json.load(response)
+    geojson = json.load(response)
 
 
-df = pd.read_csv("covid_confirmed_usafacts.csv")
 
-sample = df[["countyFIPS","stateFIPS","County Name", "10/24/20"]]
+# Plotting counties
+def plot_counties():
+    counties = pd.read_pickle("final_county.gz",compression="gzip")
+    # preprocessing that won't need be done in the final verison (should be already done in the data)
+    counties["countyFIPS"] = counties["countyFIPS"].astype(str)
+    counties['countyFIPS'] = counties['countyFIPS'].apply(lambda x: ('0'+x if(len(x) == 4) else x))
 
-sample["scaled_case"] = np.log10(sample["10/24/20"])
-#print(sample.head())
+    # getting list of dates
+    dates = counties["Date"].unique()
+    # building discrete color map
+    discrete_color = list(pd.unique(counties["Case Rate Color"]))
+    discrete_color_map = {}
+    for i in discrete_color:
+        discrete_color_map[i] = i
+        
+    # slicing data for 1 day
+    sample = counties[counties.Date == dates[100]]
+        
+    """
+    px.choropleth break down
+    
+    sample = dateframe
+    geojson = geojson (dictionary of points to draw the map)
+    locations = matches the geojson and is a way to match the data to a part of the map (example counties, which is contained in the geojson)
+    color = the data that we are visualizing
+    color_discrete_map = discrete color map for the data in color
+    scope = what to show
+    
+    """
+    fig = px.choropleth(sample,geojson=geojson,locations='countyFIPS', color='Case Rate Color',color_discrete_map=discrete_color_map, scope="usa")
+    
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
 
-fig = px.choropleth(sample, geojson=counties, locations='stateFIPS', color='scaled_case',
-                           scope="usa",
-                          )
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+# Plotting states
+def plot_states():
+    states = pd.read_pickle("final_state.gz",compression="gzip")
+    dates = states["Date"].unique()
+    sample = states[states.Date == dates[50]]
+
+    fig = px.choropleth(sample,locationmode="USA-states",locations='State', color='Case growth rate',scope="usa")
+    
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
+# 
